f7f25383404d11a1666e22a9e03147b508cc2567
jongwk <jongwk64@gmail.com>
2023-09-27 15:34:37 +0900

    week05 v27

--- week05/src/PokemonGame.java
+++ week05/src/PokemonGame.java
@@ -26,7 +26,7 @@ public class PokemonGame {
         // Pokemon player = new Pokemon();  // 추상클래스의 객체는 생성 불가
         Pokemon player = null;  // 추상클래스의 변수 선언은 가능 (upcasting 용)
         Scanner scanner = new Scanner(System.in);
-        System.out.print("포켓몬을 고르세요.\n1) 피카츄   2) 꼬부기   3)리자몽 : ");
+        System.out.print("포켓몬을 고르세요.\n1) 피카츄   2) 꼬부기   3) 리자몽 : ");
         int pokemonPick = scanner.nextInt();
         if(pokemonPick == 1){
             player = new Pikachu(new NoFly());
@@ -43,11 +43,12 @@ public class PokemonGame {
             System.out.print("\t1) 전투   2) 도망   3) 종료 : ");
             menu = scanner.nextInt();
             if(menu == 1){
-                System.out.print("전투 기술 : 1) " + player.skills[0] + "  2)" + player.skills[1]+ "  3)"
+                System.out.print("전투 기술 : 1) " + player.skills[0] + "  2) " + player.skills[1]+ "  3) "
                         + player.skills[2] +  "  :");
                 skillMenu = scanner.nextInt();
                 //player.attack(enemy, scanner.next());
-                player.attack(enemy, player.skills[skillMenu-1] );
+                //player.attack(enemy, player.skills[skillMenu-1] );
+                player.attack(enemy, skillMenu);
             }else if(menu == 2){
 
             }else{
